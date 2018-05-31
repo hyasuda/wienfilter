7f7946254db6f769a720992aff202ed5c03dcd28
Hiromasa <hyasuda.phys@gmail.com>
2018-05-31 14:30:41 +0900

    first commit

---
+++ B-Lcalc.cc
@@ -0,0 +1,14 @@
+// This macro is to calculate the B-L relation in wien filter //
+// This is updated on 31, May, 2018. by H. Yasuda //
+
+#include "fstream"
+
+const double alpha = 180; // spin rotation
+const double beta = 0.08; // velocity of muon
+const double gamma = 1/(sqrt(1-beta*beta)); // Lorentz factor
+const double m_mu = 105.66; // muon mass [MeV]
+
+
+int main(){
+
+}
